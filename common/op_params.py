93dedebcc0e48fd85bf6f0af1bed77ba550fd8ea
andy741217 <76022969+andy741217@users.noreply.github.com>
2021-01-15 22:01:33 +0800

    Update op_params.py

--- common/op_params.py
+++ common/op_params.py
@@ -111,7 +111,7 @@ class opParams:
                         'rsa_max_speed': Param(24.5, VT.number, 'Speed limit to ignore RSA in m/s'),
                         'smart_speed': Param(True, bool, 'Whether to use Smart Speed for drives above smart_speed_max_vego'),
                         'smart_speed_max_vego': Param(26.8, VT.number, 'Speed limit to ignore Smartspeed in m/s'),
-                        'speed_offset': Param(0, VT.number, 'Speed limit offset in m/s', live=True)
+                        'speed_offset': Param(0, VT.number, 'Speed limit offset in m/s', live=True),
                         'SccEnabled': Param(True, bool, 'Car has SCC Harness'),
                         'SccHarnessPresentt': Param(True, bool, 'Car has SCC Harness'),
                         'LongControlEnabled': Param(True, bool, 'only for NON SCC car, OP will send SCC message'),
